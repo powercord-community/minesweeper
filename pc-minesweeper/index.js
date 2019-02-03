071ee06d1ebac2227b06ae267a4e11ec10015628
Joakim <21Joakim@users.noreply.github.com>
2019-02-03 02:29:13 +0100

    Better mounting

--- pc-minesweeper/index.js
+++ pc-minesweeper/index.js
@@ -50,11 +50,10 @@ module.exports = class Minesweeper extends Plugin {
     BOT_AVATARS.minesweeper = 'https://i.imgur.com/LGQUFYQ.png';
 
     const _this = this;
-    const Spoiler = getModuleByDisplayName('Spoiler');
-    inject('pc-minesweeper-spoiler', Spoiler.prototype, 'componentDidMount', function (args, res) { // eslint-disable-line func-names
-      const messageComponent = _this.getParent(this, 'MessageContent');
-      const { message } = messageComponent.props;
 
+    const MessageContent = getModuleByDisplayName('MessageContent');
+    inject('pc-minesweeper-message', MessageContent.prototype, 'componentDidMount', function (args, res) { // eslint-disable-line func-names
+      const { message } = this.props;
       if (message.author.id === '1' && message.author.username === 'Minesweeper') {
         if (!message.minesweeper) {
           message.minesweeper = {
@@ -62,70 +61,73 @@ module.exports = class Minesweeper extends Plugin {
           };
         }
 
-        const spoilers = [ ...messageComponent._reactInternalFiber.child.child.stateNode.children[1].children ];
-        const child = this._reactInternalFiber.child.child.child.stateNode;
+        const spoilers = [ ...this._reactInternalFiber.child.child.stateNode.children[1].children ];
+        for (const spoiler of spoilers) {
+          const spoilerComponent = _this.getParent(getOwnerInstance(spoiler), 'Spoiler');
+          const child = spoilerComponent._reactInternalFiber.child.child.child.stateNode;
 
-        const index = spoilers.findIndex(e => e === child.ref);
+          const index = spoilers.findIndex(e => e === child.ref);
 
-        const y = Math.floor(index / message.minesweeper.tiles[0].length);
-        const x = index % message.minesweeper.tiles[0].length;
+          const y = Math.floor(index / message.minesweeper.tiles[0].length);
+          const x = index % message.minesweeper.tiles[0].length;
 
-        const tile = message.minesweeper.tiles[y][x];
+          const tile = message.minesweeper.tiles[y][x];
 
-        child.ref.addEventListener('click', (e) => {
-          if (!tile.flagged && !tile.revealed) {
-            tile.revealed = true;
-            tile.flagged = false;
+          child.ref.addEventListener('click', (e) => {
+            if (!tile.flagged && !tile.revealed) {
+              tile.revealed = true;
+              tile.flagged = false;
 
-            if (tile.type === TileType.BOMB) {
-              message.minesweeper.tiles.forEach((tiles) => {
-                tiles.forEach((t) => {
-                  t.revealed = true;
-                  t.flagged = false;
+              if (tile.type === TileType.BOMB) {
+                message.minesweeper.tiles.forEach((tiles) => {
+                  tiles.forEach((t) => {
+                    t.revealed = true;
+                    t.flagged = false;
+                  });
                 });
-              });
 
-              spoilers.forEach((element) => {
-                _this.getParent(getOwnerInstance(element), 'Spoiler').setState({ visible: true });
-              });
+                spoilers.forEach((element) => {
+                  _this.getParent(getOwnerInstance(element), 'Spoiler').setState({ visible: true });
+                });
 
-              ComponentDispatch.dispatch(ComponentActions.SHAKE_APP, {
-                duration: 800,
-                intensity: 10
-              });
+                ComponentDispatch.dispatch(ComponentActions.SHAKE_APP, {
+                  duration: 800,
+                  intensity: 10
+                });
+              }
+            } else {
+              e.preventDefault();
+              e.stopPropagation();
+              e.stopImmediatePropagation();
             }
-          } else {
+          });
+
+          child.ref.addEventListener('contextmenu', (e) => {
             e.preventDefault();
             e.stopPropagation();
             e.stopImmediatePropagation();
-          }
-        });
 
-        child.ref.addEventListener('contextmenu', (e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          e.stopImmediatePropagation();
+            if (!tile.revealed) {
+              tile.flagged = !tile.flagged;
 
-          if (!tile.revealed) {
-            tile.flagged = !tile.flagged;
-
-            if (tile.flagged) {
-              child.ref.children[0].style.display = 'none';
-              child.ref.innerHTML += '<img src="/assets/a1f0c106b0a0f68f6b11c2dc0cc8d249.svg" class="emoji" alt=":triangular_flag_on_post:" draggable="false">';
-            } else {
-              child.ref.children[0].style.display = '';
-              child.ref.children[1].remove();
+              if (tile.flagged) {
+                child.ref.children[0].style.display = 'none';
+                child.ref.innerHTML += '<img src="/assets/a1f0c106b0a0f68f6b11c2dc0cc8d249.svg" class="emoji" alt=":triangular_flag_on_post:" draggable="false">';
+              } else {
+                child.ref.children[0].style.display = '';
+                child.ref.children[1].remove();
+              }
             }
+          });
+
+          if (tile.flagged) {
+            child.ref.children[0].style.display = 'none';
+            child.ref.innerHTML += '<img src="/assets/a1f0c106b0a0f68f6b11c2dc0cc8d249.svg" class="emoji" alt=":triangular_flag_on_post:" draggable="false">';
           }
-        });
 
-        if (tile.flagged) {
-          child.ref.children[0].style.display = 'none';
-          child.ref.innerHTML += '<img src="/assets/a1f0c106b0a0f68f6b11c2dc0cc8d249.svg" class="emoji" alt=":triangular_flag_on_post:" draggable="false">';
-        }
-
-        if (tile.revealed) {
-          this.setState({ visible: true });
+          if (tile.revealed) {
+            spoilerComponent.setState({ visible: true });
+          }
         }
       }
 
